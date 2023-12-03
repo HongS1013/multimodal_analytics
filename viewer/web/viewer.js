07638a0f89132df7e746d05962f1eb6cbc675c08
HongS <41907842+HongS1013@users.noreply.github.com>
2023-12-02 19:50:10 -0600

    Update viewer.js

--- viewer/web/viewer.js
+++ viewer/web/viewer.js
@@ -4119,7 +4119,7 @@ var defaultOptions = {
     kind: OptionKind.VIEWER + OptionKind.PREFERENCE
   },
   defaultUrl: {
-    value: 'siemens-2013-learning-analytics-the-emergence-of-a-discipline.pdf',
+    value: '2019-Sharma.pdf',
     kind: OptionKind.VIEWER
   },
   defaultZoomValue: {
